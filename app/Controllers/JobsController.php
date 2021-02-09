bb227d1b6da8e1de3fb0b932637137c7f773ae3d
Felps1909 <felipe.m1909@hotmail.com>
2021-02-08 22:36:11 -0300

    JobsController Funcionando

--- app/Controllers/JobsController.php
+++ app/Controllers/JobsController.php
@@ -1,30 +1,15 @@
 <?php
-
-// namespace App\Controllers;
-
-// use App\Models\Job;
-// use Illuminate\Http\Request;
-
-// use App\Http\Requests;
-// use App\Http\Controllers\Controller;
-
-// class JobsController extends Controller
-// {
-//     public function index()
-//     {
-//         $jobs = Job::with('company')->get();
-//         return response()->json($jobs);
-//     }
-// }
-
 namespace App\Controllers;
 
-use App\Controllers\Controller;
+use Illuminate\Routing\Controller;
+use App\Models\Job;
 
 class JobsController extends Controller
 {
     public function index()
-    {
-        die("Oi");
-    }
+        {
+            $jobs = Job::with('company')->get();
+            return response()->json($jobs);
+            
+        }
 }
